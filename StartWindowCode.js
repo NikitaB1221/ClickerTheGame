dd1e7910792336cf449d1063119b6d6f7df21767
NikitaB1221 <44338356+NikitaB1221@users.noreply.github.com>
2023-05-02 21:01:06 +0300

    Add files via upload

--- StartWindowCode.js
+++ StartWindowCode.js
@@ -2,16 +2,25 @@
 let username, score = "";
 let userDMG = 20;
 
-
-const palyButtonClick = () =>{
-    if(usenameTextArea.value === "" || usenameTextArea.value === " ") {
+const playButtonClick = () => {
+    if (usenameTextArea.value === "" || usenameTextArea.value === " ") {
         postMessage("Username can't be empty!")
-        return; }
-    
-    username = usenameTextArea.value;   
+        return;
+    }
+
+    username = usenameTextArea.value;
     usenameTextArea.value = "";
+    document.getElementById('playButton').style.display = "none";
+    document.getElementById('usenameTextArea').style.display = "none";
+    document.getElementById('startWindow').style.display = "none";
+};
+
+window.onload=function(){
+    let pB = document.getElementById('playButton');
+    pB.addEventListener("click", playButtonClick);
 }
 
-const SaveResult = () =>{
+
+const SaveResult = () => {
     localStorage.setItem(username, score);
 };
