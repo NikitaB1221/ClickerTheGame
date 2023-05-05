3ccf1e25c453100f283536448fdd6b6074a66cf0
Revelioti <128666820+Revelioti@users.noreply.github.com>
2023-05-05 12:30:57 +0300

    V0.9

--- ProjectCode.js
+++ ProjectCode.js
@@ -7,6 +7,7 @@
     let playerDMG = 20;
     let lvlCount = 0;
     let audio = document.getElementById('background-music');
+    let loose = false;
 
     document.getElementById('weapon').hidden = true;
     document.getElementById('shield').hidden = true;
@@ -43,9 +44,11 @@
 
     // New level
     function IsNewLevel() {
-        if (document.body.querySelectorAll('.entityContainer').length === 0) {
+        if (document.body.querySelectorAll('.entityContainer').length === 0 && loose === false) {
 
             lvlCount++;
+            document.getElementById('userHpBar').value+=20;
+            document.getElementById('userStaminaBar').value=100;
             if (lvlCount > 3) lvlCount = 1;
 
             switch (lvlCount) {
@@ -60,8 +63,9 @@
                     break;
             }
 
-            for (let i = 0; i < Math.floor(Math.random() * 100 % 5 + 2); i++) {
-                new Enemy1().Create();
+            for (let i = 0; i < Math.floor(Math.random() * 100 % 3 + 4); i++) {
+                if((Math.floor(Math.random() * 10 % 6)) === 1) new Enemy2().Create();
+                else new Enemy1().Create();
             }
         }
     }
@@ -74,12 +78,12 @@
         animCount++;
 
         const anim = `@keyframes anim_${animCount} {
-            0% { left: ${Math.floor(Math.random() * 100 % 70 + 10)}%; top: ${Math.floor(Math.random() * 100 % 50 + 10)}%; }
-            20% { left: ${Math.floor(Math.random() * 100 % 70 + 10)}%; top: ${Math.floor(Math.random() * 100 % 50 + 10)}%; }
-            40% { left: ${Math.floor(Math.random() * 100 % 70 + 10)}%; top: ${Math.floor(Math.random() * 100 % 50 + 10)}%; }
-            60% { left: ${Math.floor(Math.random() * 100 % 70 + 10)}%; top: ${Math.floor(Math.random() * 100 % 50 + 10)}%; }
-            80% { left: ${Math.floor(Math.random() * 100 % 70 + 10)}%; top: ${Math.floor(Math.random() * 100 % 50 + 10)}%; }
-            100% { left: ${Math.floor(Math.random() * 100 % 70 + 10)}%; top: ${Math.floor(Math.random() * 100 % 50 + 10)}%; }
+            0% { left: ${Math.floor(Math.random() * 100 % 70 + 10)}%; top: ${Math.floor(Math.random() * 100 % 50 + 5)}%; }
+            20% { left: ${Math.floor(Math.random() * 100 % 70 + 10)}%; top: ${Math.floor(Math.random() * 100 % 50 + 5)}%; }
+            40% { left: ${Math.floor(Math.random() * 100 % 70 + 10)}%; top: ${Math.floor(Math.random() * 100 % 50 + 5)}%; }
+            60% { left: ${Math.floor(Math.random() * 100 % 70 + 10)}%; top: ${Math.floor(Math.random() * 100 % 50 + 5)}%; }
+            80% { left: ${Math.floor(Math.random() * 100 % 70 + 10)}%; top: ${Math.floor(Math.random() * 100 % 50 + 5)}%; }
+            100% { left: ${Math.floor(Math.random() * 100 % 70 + 10)}%; top: ${Math.floor(Math.random() * 100 % 50 + 5)}%; }
           }`;
 
         var styleSheet = document.createElement("style");
@@ -104,6 +108,7 @@
             enemyContainer.style = AnimData;
             enemyContainer.addEventListener('click', EnemyDamage);
             enemyContainer.setAttribute('atCd', Math.floor(Math.random() * 10 % 8 + 2));
+            enemyContainer.setAttribute('atDm', Math.floor(Math.random() * 100 % 10 + 20));
             document.body.append(enemyContainer);
 
             const prBar = document.createElement('progress');
@@ -127,6 +132,44 @@
         }
     }
 
+    class Enemy2 {
+        Create() {
+            let PosX = "top: " + Math.floor(Math.random() * 100 % 50) + "%;";
+            let PosY = "right: " + Math.floor(Math.random() * 100 % 50) + "%;";
+
+            let AnimData = "animation: " + Math.floor((Math.random() * 100 % 4) + 10) + "s " + CreateAnim() + " infinite alternate;";
+
+            const enemyContainer = document.createElement('div');
+            enemyContainer.classList.add('entityContainer');
+            enemyContainer.style = PosX;
+            enemyContainer.style = PosY;
+            enemyContainer.style = AnimData;
+            enemyContainer.addEventListener('click', EnemyDamage);
+            enemyContainer.setAttribute('atCd', Math.floor(Math.random() * 10 % 3 + 6));
+            enemyContainer.setAttribute('atDm', Math.floor(Math.random() * 100 % 60 + 40));
+            document.body.append(enemyContainer);
+
+            const prBar = document.createElement('progress');
+            prBar.setAttribute('max', '600');
+            prBar.setAttribute('value', '600');
+            prBar.id = 'prBar';
+            enemyContainer.append(prBar);
+
+            const entity = document.createElement('div');
+            entity.classList.add('Entity2');
+            entity.style.backgroundImage = 'url(en2.gif)';
+            entity.style.backgroundSize = 'contain';
+
+            let color = Math.floor(Math.random() * 1000 % 100);
+
+            let colorString = 'hue-rotate(' + color + 'deg)';
+            enemyContainer.setAttribute('color', color);
+            entity.style.filter = colorString;
+            enemyContainer.append(entity);
+            entity.addEventListener('click', PlayerDamage);
+        }
+    }
+
 
 
 
@@ -137,22 +180,49 @@
         IsNewLevel();
     }
 
-    function PlayerDamage() {
+    function PlayerDamage(dmg) {
+        document.getElementById('userHpBar').value-=dmg;
+        if(document.getElementById('userHpBar').value < 1) {
+            loose = true;
+            document.getElementById('weapon').hidden = true;
+            document.getElementById('shield').hidden = true;
+            document.body.style.backgroundImage ='none';
+            document.body.style.backgroundColor ='black';
+            let enemyList = document.body.querySelectorAll('.entityContainer');
+            for (let i = 0; i < enemyList.length; i++) {
+                enemyList[i].remove();
+            }
+        }
+    }
 
+    function ShieldDamage(dmg) {
+        document.getElementById('userStaminaBar').value-=dmg;
+        if(document.getElementById('userStaminaBar').value < 1) {
+            document.getElementById('userStaminaBar').value += 50;
+            document.getElementById('userHpBar').value -= 20;
+        }
     }
 
     function AttackTime() {
         timer++;
+
+        if( document.getElementById('userStaminaBar').value < 100 && shieldUp === false)  document.getElementById('userStaminaBar').value += 5;
+
         let enemyList = document.body.querySelectorAll('.entityContainer');
         for (let i = 0; i < enemyList.length; i++) {
             if (timer % enemyList[i].getAttribute('atCd') === 0) {
 
                 const attackMarker = document.createElement('img');
 
-                if (enemyList[i].className === "entityContainer") {
+                if (enemyList[i].childNodes[1].className === "Entity") {
                     attackMarker.style.height = '200px';
                     attackMarker.style.width = '300px';
                     attackMarker.style.backgroundImage = 'url(at1.png)';
+                }
+                else{
+                    attackMarker.style.height = '250px';
+                    attackMarker.style.width = '250px';
+                    attackMarker.style.backgroundImage = 'url(at2.png)';
                 }
 
                 attackMarker.style.position = 'absolute';
@@ -164,7 +234,9 @@
                 enemyList[i].childNodes[1].append(attackMarker);
             }
             else if (enemyList[i].getAttribute('atCd') % timer !== 0 && enemyList[i].childNodes[1].childNodes.length > 0) {
-                enemyList[i].childNodes[1].removeChild(enemyList[i].childNodes[1].lastChild)
+                enemyList[i].childNodes[1].removeChild(enemyList[i].childNodes[1].lastChild);
+                if(shieldUp === false) PlayerDamage(enemyList[i].getAttribute('atDm'));
+                else(ShieldDamage(enemyList[i].getAttribute('atDm')/2));
             }
         }
     }
@@ -176,7 +248,7 @@
     // Animations
     document.body.addEventListener('keydown', function (event) {
         let shield = document.getElementById('shield');
-        if (event.code === 'ShiftLeft') {
+        if (event.code === 'ShiftLeft' && loose === false) {
             shield.src = 'ws2.png';
             shield.style.left = '25%';
             document.getElementById('weapon').hidden = true;
@@ -186,7 +258,7 @@
 
     document.body.addEventListener('keyup', function (event) {
         let shield = document.getElementById('shield');
-        if (event.code === 'ShiftLeft') {
+        if (event.code === 'ShiftLeft'&& loose === false) {
             shield.src = 'ws1.png';
             shield.style.left = '5%';
             document.getElementById('weapon').hidden = false;
